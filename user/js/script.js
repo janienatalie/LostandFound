eaf863412a6a8804b2e7fd8bd32e679101b6e14a
janienatalie <janienataliee@gmail.com>
2024-12-16 01:19:44 +0700

    ubah border

--- user/js/script.js
+++ user/js/script.js
@@ -1,7 +1,14 @@
 const dropdown = document.getElementById("dropdown");
+const dropdowncampus = document.getElementById("dropdowncampus");
 
 dropdown.addEventListener("change", function () {
   if (dropdown.value) {
     dropdown.style.color = "#763996";
+  }
+});
+
+dropdowncampus.addEventListener("change", function () {
+  if (dropdowncampus.value) {
+    dropdowncampus.style.color = "#763996";
   }
 });
