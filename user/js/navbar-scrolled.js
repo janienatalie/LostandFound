42174c7b27c9b43d82e7b71ba51aa7fd50010a5a
Delia-Anjani <deliadwianjani@gmail.com>
2025-01-03 20:40:59 +0700

    navbarjs, home, navbar, home css

---
+++ user/js/navbar-scrolled.js
@@ -0,0 +1,10 @@
+// navbar-scroll.js
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+  
