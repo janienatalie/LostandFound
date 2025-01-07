c255f05f85bb60243a784f0282484576c512e984
Delia-Anjani <deliadwianjani@gmail.com>
2025-01-07 21:48:23 +0700

    home

--- user/home.php
+++ user/home.php
@@ -7,11 +7,13 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lost & Found - Gunadarma University</title>
-    <link rel="stylesheet" href="./css/home.css">
-    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="/css/style.css"/>
+    <script src="navbar-scrolled.js"></script>
+    <!--<script src="navbar-active.js"></script>-->
     
 </head>
 <body>
