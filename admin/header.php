691f12f20286a97b91f7c31663dd1267be61b102
janienatalie <janienataliee@gmail.com>
2025-01-12 19:57:04 +0700

    sisanya

---
+++ admin/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/admin.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- loading bar -->
+    <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Lost and Found - Admin</title>
+</head>
+<body>
+    <!-- mobile view -->
+    <div id="mobileview">
+        <h5>Admin panel doesn't show in mobile view</h5>
+    </div>
--- database/config.php
+++ database/config.php
@@ -5,10 +5,17 @@ $user = "root"; // Username database
 $password = ""; // Password database
 $database = "lostandfound"; // Nama database
 
-$conn = new mysqli($host, $user, $password, $database);
-
-// Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+try {
+    $conn = new mysqli($host, $user, $password, $database);
+    
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+    
+    // Set charset to handle Indonesian characters properly
+    $conn->set_charset("utf8mb4");
+} catch (Exception $e) {
+    error_log("Database connection error: " . $e->getMessage());
+    die("Database connection failed");
 }
 ?>
